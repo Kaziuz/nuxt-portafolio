f3ffb87a18270cbac62a0ddbe7d6a52407b5b3a6
johnny sepulveda <user.email>
2023-10-15 00:09:05 -0500

    first commit

---
+++ app.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { ref } from 'vue'
+const openModal1 = ref(false)
+const openModal2 = ref(false)
+
+const closeModal1 = () => {
+  openModal1.value = false
+}
+
+const closeModal2 = () => {
+  openModal2.value = false
+}
+
+</script>
+<template>
+  <div>
+    <!-- <NuxtWelcome /> -->
+    <div class="flex flex-col justify-between min-h-screen bg-gray-100 w-100">
+      <div class="">Hola. Soy johnny, bienvenido!</div>
+      <div class="flex flex-col items-center space-y-4 md:flex-row md:space-x-4 md:space-y-0 bg-yellow-200 w-100">
+        <button
+          class="w-32 h-32 bg-green-500"
+          @click="openModal1 = true"
+        >
+          texto1
+        </button>
+        <button
+          class="w-32 h-32 bg-green-500"
+          @click="openModal2 = true"
+        >
+          texto 1
+        </button>
+      </div>
+      <div>
+          Soy una persona apasionada por las computadoras, considerándolas una herramienta excelente que nos permite crear y resolver numerosos problemas, especialmente en el ámbito de la web.
+          En la actualidad, se presentan muchos desafíos para desarrollar una interfaz de usuario eficaz y me entusiasma encontrar soluciones a estos problemas.
+          Me caracterizo por estar en constante aprendizaje y mantenerme al día con las últimas tecnologías en torno al desarrollo front end.
+          Además, disfruto creando prototipos con sistemas embebidos como Arduino y ESP8266.
+          En mi tiempo libre, practico skateboarding y me creo música con sintetizadores e instrumentos musicales.
+          Creo firmemente que la combinación de tecnología, deporte y arte enriquece mi espiritu y potencia mi creatividad</div>
+      </div>
+    </div>
+  <!-- modal -->
+  <Modal :statusModal="openModal1" :closeModal="closeModal1" />
+  <Modal :statusModal="openModal2" :closeModal="closeModal2" />
+</template>
---
+++ components/Modal.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    closeModal: (event: MouseEvent) => void,
+    statusModal: boolean
+  }>
+  (), {
+    statusModal: false
+  }
+)
+
+</script>
+
+<template>
+  <div
+    v-if="props.statusModal"
+    class="absolute inset-0 flex items-center justify-center bg-gray-700 bg-opacity-50"
+  >
+    <div
+      class="max-w-2xl p-6 bg-white rounded-md shadow-xl bg-blue-900"
+    >
+      hola
+      <button
+        class="px-6 py-2 text-white border border-blue-600 rounded bg-red-500"
+        @click="props.closeModal"
+      >
+        cerrar
+      </button>
+    </div>
+    
+  </div>
+</template>
