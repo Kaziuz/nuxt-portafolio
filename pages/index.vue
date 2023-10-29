226f540152cfe016ef959d4d1543c3a06d81e7a9
johnny sepulveda <user.email>
2023-10-29 16:19:42 -0500

    minor changes, solve fix warnings ts

--- pages/index.vue
+++ pages/index.vue
@@ -1,4 +1,6 @@
 <script setup lang="ts">
+import { ref, onMounted } from 'vue'
+
 interface HomeData {
   principal: string
   subPrincipal: string
@@ -8,35 +10,38 @@ interface HomeData {
   projects: string
   infoFooter: string
 }
+const homeDataView = ref<HomeData | null>(null)
 
-const fetchHomeData = await queryContent('/home').findOne()
+onMounted(async () => {
+  const fetchHomeData = await queryContent('/home').findOne()
 
-const homeDataView: HomeData = {
-  principal: fetchHomeData.principal,
-  subPrincipal: fetchHomeData.subPrincipal,
-  circle: fetchHomeData.circle,
-  description: fetchHomeData.description,
-  habilities: fetchHomeData.habilities,
-  projects: fetchHomeData.projects,
-  infoFooter: fetchHomeData.infoFooter,
-}
+  homeDataView.value = {
+    principal: fetchHomeData.principal,
+    subPrincipal: fetchHomeData.subPrincipal,
+    circle: fetchHomeData.circle,
+    description: fetchHomeData.description,
+    habilities: fetchHomeData.habilities,
+    projects: fetchHomeData.projects,
+    infoFooter: fetchHomeData.infoFooter,
+  }
+})
 
 </script>
 <template>
   <section class="mx-4 md:mx-60 flex flex-col justify-evenly min-h-screen w-100">
     <h5 class="color-text title text-6xl uppercase text-center md:text-left">
       <p class="text-xl mt-4 md:-mt-2 tracking-wider">
-        {{ homeDataView.principal }}
+        {{ homeDataView?.principal }}
       </p>
       <p class="capitalize">
-        {{ homeDataView.subPrincipal }}
+        {{ homeDataView?.subPrincipal }}
       </p>
     </h5>
     <article class="h-64 max-h-72 md:h-56 md:max-h-64 flex justify-center">
       <div class="flex flex-col items-center justify-center space-y-14 md:flex-row md:space-x-32 md:space-y-0 w-100">
         <button class="circle pointer-events-none">
           <span class="title text-md uppercase tracking-wider">
-            {{ homeDataView.circle }}
+            {{ homeDataView?.circle }}
           </span>
         </button>
       </div>
@@ -51,10 +56,10 @@ const homeDataView: HomeData = {
         </a>
       </div>
       <p class="subtitle text-justify">
-        {{ homeDataView.description }}
+        {{ homeDataView?.description }}
       </p>
       <div class="subtitle flex items-center mt-4 md:mt-6 space-x-3">
-        <p class="subtitle text-sm">{{ homeDataView.habilities }}</p>
+        <p class="subtitle text-sm">{{ homeDataView?.habilities }}</p>
         <IconHtml
           class="transition-all duration-500 ease-in-out transform hover:scale-125"
           size="30px"
@@ -81,16 +86,28 @@ const homeDataView: HomeData = {
         />
       </div>
       <div class="subtitle flex items-center mt-4 md:mt-6 space-x-3">
-        <p class="subtitle text-sm">{{ homeDataView.projects }}</p>
-        <a class="hover:text-sky-600 italic"
-          target="_blank"
-          href="https://www.malahierbatropical.com/radio/"
-        >
-         * MalaHierba *
-        </a>
+        <p class="subtitle text-sm">{{ homeDataView?.projects }}</p>
+        <div class="flex items-center align-center flex-col">
+          <a class="hover:text-sky-600 italic"
+            target="_blank"
+            href="https://www.malahierbatropical.com/radio/"
+          >
+           * MalaHierba *
+          </a>
+          <div class="text-sm">Vue3, Leaflet, Vuex, TailwindCSS</div>
+        </div>
+        <div class="flex items-center align-center flex-col">
+          <a class="hover:text-sky-600 italic"
+            target="_blank"
+            href="https://bestllook.netlify.app/"
+          >
+           * Bestllook *
+          </a>
+          <div class="text-sm">Html, Css</div>
+        </div>
       </div>
       <p class="color-text text-center subtitle text-sm">
-        {{ homeDataView.infoFooter }}
+        {{ homeDataView?.infoFooter }}
       </p>
     </article>
   </section>
